d9fbe98f3dcb5be166f4cade82cb224c6e8aa894
fgsreally <1325041831@qq.com>
2023-08-18 18:04:38 +0800

    feat(cli): add documentPicInPic

--- packages/cli/src/helper/merak.ts
+++ packages/cli/src/helper/merak.ts
@@ -1,12 +1,13 @@
 import type { HtmlTag } from '../types'
 
-export function documentPicInPic(selector: string, options?: any): HtmlTag {
+export function documentPicInPic(text: string, selector: string, options?: any): HtmlTag {
   return {
     injectTo: 'body',
     tag: `<script type="module">const el = document.querySelector(\`${selector}\`);
     if(window.documentPictureInPicture){
       const btn=document.createElement('button')
-      btn.innerHTML='CLICK'
+      btn.innerHTML='${text}'
+      btn.style='position:fixed;z-index:100;'
       document.body.append(btn)
       btn.onclick=()=>{
         documentPictureInPicture.requestWindow(${options ? JSON.stringify(options) : ''}).then((w)=>{
